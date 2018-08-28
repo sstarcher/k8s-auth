d4bcbaa656ab3d3c94ca8695abba0cd4c940ae07
Shane Starcher <shanestarcher@gmail.com>
2018-08-28 09:29:05 -0700

    adding cache for token expiration and improving logging

---
+++ version.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use: "version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("%v, commit %v, built at %v", version, commit, date)
+	},
+}
